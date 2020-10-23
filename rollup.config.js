5134d7a97e189944c4a2d03479681c2be5a23a5b
xiaolin <462441355@qq.com>
2020-10-23 22:45:22 +0800

    * 测试

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import serve from 'rollup-plugin-serve'
+
 // rollup可以帮我们打包 es6的模块化语法
 export default {
     input: './src/index.js',
@@ -6,5 +8,12 @@ export default {
         format: 'umd',
         name: 'datav',
         sourcemap: true
-    }
+    },
+    plugins: [
+        serve({
+            openPage: './index.html',
+            contentBase: '',
+            port: 9002
+        }),
+    ]
 }
