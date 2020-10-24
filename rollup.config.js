490e3f3262f5e1e3ec3769771bb631975a82e554
xiaolin <462441355@qq.com>
2020-10-24 16:26:05 +0800

    * 增加注释

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import serve from 'rollup-plugin-serve'
 import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
-// const resolve = require('rollup-plugin-node-resolve') 
+import resolve from 'rollup-plugin-node-resolve' // 外部依赖打包到项目
 
 // rollup可以帮我们打包 es6的模块化语法
 export default {
