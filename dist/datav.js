745ae73a2c18b9bc0ddd056023cdf4ae2c1db4c5
xiaolin <462441355@qq.com>
2020-10-24 17:01:53 +0800

    * add

--- dist/datav.js
+++ dist/datav.js
@@ -8,16 +8,7 @@
       a: 1
     };
 
-    function random(base) {
-      if (base && base % 1 === 0) {
-        return Math.floor(Math.random() * base);
-      } else {
-        return 0;
-      }
-    }
-
-    console.log(title, random);
-    console.log(random());
+    console.log(title);
     var index = {};
 
     return index;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import title from './title'
-import { random } from 'sam-test-data'
-console.log(title, random)
-console.log(random())
+console.log(title)
 export default {}
