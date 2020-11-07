c293080a398472702bb4cf27e43216fcb4728229
xiaolin <462441355@qq.com>
2020-11-07 20:42:47 +0800

    * 完善icon 组件

--- src/components/Icon/icon.vue
+++ src/components/Icon/icon.vue
@@ -1,7 +1,9 @@
 <template>
-    <svg :style="{...style}">
-        <use :href="iconName"></use>
-    </svg>
+    <div class="icon-wrapper" :style="{...style}">
+        <svg class="icon">
+            <use :href="iconName"></use>
+        </svg>
+    </div>
 </template>
 
 <script>
@@ -9,13 +11,30 @@ export default {
     name: 'IconComponent',
     props: {
         name: String,
+        prefix: {
+            type: String,
+            default: 'icon'
+        },
         style: Object
     },
     setup(ctx) {
-        const iconName = `#${ctx.name}`
+        const iconName = `#${ctx.prefix}${ctx.name}`
         return {
             iconName
         }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.icon-wrapper {
+    display: inline-block;
+}
+.icon {
+  width: 100%;
+  height: 100%;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
