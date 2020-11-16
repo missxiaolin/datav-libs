40036a5157192e97c24dcba580048834d4414d9c
xiaolin <462441355@qq.com>
2020-11-16 22:37:00 +0800

    * add

--- src/components/FlyBox/FlyBox.vue
+++ src/components/FlyBox/FlyBox.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>ssss</div>
+    <div class="fly-box">
+        <svg width="400" height="400">
+            <path
+                d="M5 5 L395 5 L395 395 L5 395 Z"
+                stroke-width="1"
+                stroke="#235fa7"
+                fill="none"
+            ></path>
+        </svg>
+    </div>
 </template>
 
 <script>
@@ -7,3 +16,7 @@ export default {
     name: "FlyBox"
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
