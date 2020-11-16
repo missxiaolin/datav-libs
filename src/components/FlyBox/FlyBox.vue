983d169bc7695eeb1adeae1e38ddcdeb263db962
xiaolin <462441355@qq.com>
2020-11-16 23:40:48 +0800

    * 流星效果

--- src/components/FlyBox/FlyBox.vue
+++ src/components/FlyBox/FlyBox.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="fly-box">
-    <svg width="400" height="400">
+    <svg width="400" height="400" style="background: #333">
       <defs>
         <path id="fly-box-path" d="M5 5 L395 5 L395 395 L5 395 Z"></path>
+        <radialGradient
+          id="radial-gradient"
+          cx="50%"
+          cy="50%"
+          fx="100%"
+          fy="50%"
+          r="50%"
+        >
+          <stop offset="0%" stop-color="#fff" stop-opacity="1"></stop>
+          <stop offset="100%" stop-color="#fff" stop-opacity="0"></stop>
+        </radialGradient>
         <mask id="fly-box-mask">
-            <circle r="50" cx="0" cy="0" fill="white">
-                <animateMotion
-                    dur="3s"
-                    path="M5 5 L395 5 L395 395 L5 395 Z"
-                    rotate="auto"
-                    repeatCount="indefinite"
-                ></animateMotion>
-            </circle>
+          <circle r="150" cx="0" cy="0" fill="url(#radial-gradient)">
+            <animateMotion
+              dur="3s"
+              path="M5 5 L395 5 L395 395 L5 395 Z"
+              rotate="auto"
+              repeatCount="indefinite"
+            ></animateMotion>
+          </circle>
         </mask>
       </defs>
       <use
