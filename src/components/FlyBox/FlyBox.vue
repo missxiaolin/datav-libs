8578c88da0666bf688ea0b4fd085a331385736f5
xiaolin <462441355@qq.com>
2020-11-16 22:31:42 +0800

    * 初始化组件

---
+++ src/components/FlyBox/FlyBox.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>ssss</div>
+</template>
+
+<script>
+export default {
+    name: "FlyBox"
+}
+</script>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Svg from './components/Svg/index'
 import Incon from './components/Icon/index'
 import SvgAnimation from './components/SvgAnimation/index'
 import Loading from './components/Loading/index'
+import FlyBox from './components/FlyBox/index'
 
 export default function(Vue) {
     Vue.use(Svg)
     Vue.use(Incon)
     Vue.use(SvgAnimation)
     Vue.use(Loading)
+    Vue.use(FlyBox)
 }
