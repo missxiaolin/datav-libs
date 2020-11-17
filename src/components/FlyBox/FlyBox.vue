691c02ef2516b3b9858ab2284ad6ba606574ac5f
xiaolin <462441355@qq.com>
2020-11-17 08:04:06 +0800

    * 自适应

--- src/components/FlyBox/FlyBox.vue
+++ src/components/FlyBox/FlyBox.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="fly-box">
-    <svg width="400" height="400" style="background: #333">
+  <div class="fly-box" :ref="refName">
+    <svg :width="width" :height="height">
       <defs>
-        <path id="fly-box-path" d="M5 5 L395 5 L395 395 L5 395 Z"></path>
+        <path id="fly-box-path" :d="path"></path>
         <radialGradient
           id="radial-gradient"
           cx="50%"
@@ -18,7 +18,7 @@
           <circle r="150" cx="0" cy="0" fill="url(#radial-gradient)">
             <animateMotion
               dur="3s"
-              path="M5 5 L395 5 L395 395 L5 395 Z"
+              :path="path"
               rotate="auto"
               repeatCount="indefinite"
             ></animateMotion>
@@ -39,14 +39,62 @@
         mask="url(#fly-box-mask)"
       ></use>
     </svg>
+    <div class="fly-box-content">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
+import { computed, ref, onMounted, getCurrentInstance } from "vue";
+
 export default {
   name: "FlyBox",
+  setup(ctx) {
+    const width = ref(0);
+    const height = ref(0);
+    const refName = 'fly-box'
+    const path = computed(() => {
+        return `M5 5 L${width.value - 5} 5 L${width.value - 5} ${height.value - 5} L5 ${height.value - 5} Z`
+    })
+
+    const init = () => {
+        const instance = getCurrentInstance()
+        const dom = instance.ctx.$refs[refName]
+        console.log(dom.clientWidth,dom.clientHeight)
+        width.value = dom.clientWidth
+        height.value = dom.clientHeight
+    }
+
+    onMounted(() => {
+        init()
+    })
+
+    return {
+      width,
+      height,
+      refName,
+      path
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.fly-box {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  svg {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .fly-box-content {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
