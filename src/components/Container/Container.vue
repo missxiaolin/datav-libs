94a86ecdeb4f14a827fa64515a2a797d6a2527be
xiaolin <462441355@qq.com>
2020-11-21 21:06:49 +0800

    * add

---
+++ src/components/Container/Container.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        sss
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'container'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Incon from './components/Icon/index'
 import SvgAnimation from './components/SvgAnimation/index'
 import Loading from './components/Loading/index'
 import FlyBox from './components/FlyBox/index'
+import Container from './components/Container/index'
 
 export default function(Vue) {
     Vue.use(Svg)
@@ -10,4 +11,5 @@ export default function(Vue) {
     Vue.use(SvgAnimation)
     Vue.use(Loading)
     Vue.use(FlyBox)
+    Vue.use(Container)
 }
