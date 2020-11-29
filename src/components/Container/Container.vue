9cfd2bb4ecd096b1556f2ca9d0bf6f6d6c5c0b64
xiaolin <462441355@qq.com>
2020-11-29 21:26:48 +0800

    * 容器初始化

--- src/components/Container/Container.vue
+++ src/components/Container/Container.vue
@@ -1,16 +1,49 @@
 <template>
-    <div class="container">
-        <slot></slot>
-    </div>
+  <div class="container" :ref="refName">
+    <slot></slot>
+  </div>
 </template>
 
 
 <script>
+import { ref, getCurrentInstance, onMounted } from "vue";
+
 export default {
-    name: 'container'
-}
+  name: "container",
+  props: {
+    options: Object,
+  },
+  setup(ctx) {
+    let refName = "container";
+    const width = ref(0);
+    const height = ref(0);
+    const originalWidth = ref(0);
+    const originalHeight = ref(0);
+
+    onMounted(() => {
+      const context = getCurrentInstance().ctx;
+      const dom = context.$refs[refName];
+      if (!!ctx.options && !!ctx.options.width && !!ctx.options.height) {
+        width.value = ctx.options.width;
+        height.value = ctx.options.height;
+      } else {
+        width.value = dom.clientWidth;
+        height.value = dom.clientHeight;
+      }
+      if (!originalWidth.value || !originalHeight.value) {
+        originalWidth.value = window.screen.width;
+        originalHeight.value = window.screen.height;
+      }
+    });
+
+    return {
+      refName,
+      width,
+      height,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
