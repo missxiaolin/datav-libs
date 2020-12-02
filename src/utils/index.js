d5b40fd4b86acdecc91835d97ca72356840d8f12
xiaolin <462441355@qq.com>
2020-12-02 22:39:39 +0800

    * 获取唯一uuid

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 function debounce(delay, calback) {
     let task
-    return function() {
+    return function () {
         clearTimeout(task)
         task = setTimeout(() => {
             calback.apply(this, arguments)
@@ -8,6 +8,19 @@ function debounce(delay, calback) {
     }
 }
 
+function generateUUID() {
+    let uuid = ''
+    let d = new Date().getTime()
+    uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        let r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+
+    return uuid
+}
+
 export {
-    debounce
+    debounce,
+    generateUUID
 }
