0d3adb2a38402fc63275ed68314c486ca55f8ad5
xiaolin <462441355@qq.com>
2020-12-05 23:17:50 +0800

    * 初始化样式

--- src/components/AverageAge/AverageAge.vue
+++ src/components/AverageAge/AverageAge.vue
@@ -1,13 +1,137 @@
 <template>
-  <div>ssss</div>
+  <div class="average-age">
+    <div class="title-wrapper">
+      <div class="average-age-left">
+        <div class="title">外卖用户年龄分布&平均年龄</div>
+        <div class="sub-title">Distribution of Age</div>
+      </div>
+      <div class="average-age-right">
+        <div class="age">
+          <vue-count-to
+            :start-val="0"
+            :end-val="100"
+            :duration="1000"
+            :decimals="2"
+          />
+          <span class="age-unit">岁</span>
+        </div>
+      </div>
+    </div>
+    <div id="average-age-chart">
+      <vue-echarts :options="options" />
+    </div>
+    <div class="average-data-wrapper">
+      <div class="average-data" v-for="(item, index) in data" :key="index">
+        <div class="average-data-value">
+          <vue-count-to
+            :start-val="item.startValue"
+            :end-val="item.value"
+            :duration="1000"
+          />
+        </div>
+        <div class="average-data-axis">
+          <div class="point" :style="{ background: item.color }" />
+          <div class="text">{{ item.axis }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
 export default {
   name: "AverageAge",
+  props: {
+    data: {
+      type: Array,
+      default: [],
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.average-age {
+  width: 100%;
+  height: 100%;
+  background: rgb(43, 44, 46);
+  padding: 20px 40px;
+  box-sizing: border-box;
+
+  .title-wrapper {
+    display: flex;
+    align-items: center;
+
+    .average-age-left {
+      .title {
+        font-size: 32px;
+      }
+
+      .sub-title {
+        font-size: 16px;
+        margin-top: 10px;
+      }
+    }
+
+    .average-age-right {
+      flex: 1;
+      margin-left: 40px;
+      font-weight: bold;
+
+      .age {
+        font-size: 68px;
+        font-family: DIN;
+
+        .age-unit {
+          font-size: 20px;
+        }
+      }
+    }
+  }
+
+  #average-age-chart {
+    height: 120px;
+  }
+
+  .average-data-wrapper {
+    display: flex;
+
+    .average-data {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-size: 30px;
+      font-weight: bolder;
+
+      .average-data-value {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+      }
+
+      .average-data-axis {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        margin-top: 5px;
+
+        .point {
+          width: 10px;
+          height: 10px;
+          border-radius: 50%;
+        }
+
+        .text {
+          margin-left: 10px;
+          font-size: 16px;
+        }
+      }
+    }
+  }
+}
 </style>
