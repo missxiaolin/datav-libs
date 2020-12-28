9193d4b4278e42b337279a15ce029d1e8c15f432
xiaolin <462441355@qq.com>
2020-12-28 23:22:40 +0800

    * header 样式初始化

--- src/components/BaseScrollList/BaseScrollList.vue
+++ src/components/BaseScrollList/BaseScrollList.vue
@@ -1,21 +1,60 @@
 <template>
-    <div class="base-scroll-list">
-        11111
+  <div class="base-scroll-list">
+    <div
+      class="base-scroll-list-header"
+      :style="{ backgroundColor: headerBg, height: `${headerHeight}px` }"
+    >
+      <div
+        class="header-item base-scroll-list-text"
+        v-for="(headerItem, i) in header"
+        :key="i"
+        :style="headerStyle[i]"
+        v-html="headerItem"
+      ></div>
     </div>
+    <div class="base-scroll-list-rows"></div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'BaseScrollList',
-    props: {
-        
+  name: "BaseScrollList",
+  props: {
+    // 标题数据 [a, b, c]
+    header: Array,
+    // 标题样式  [{}, {}, {}]
+    headerStyle: Array,
+    headerBg: {
+      type: String,
+      default: "rgb(90, 90, 90)",
     },
-    setup(ctx) {
-        
-    }
-}
+    headerHeight: {
+        type: [String, Number],
+        default: 35
+    },
+  },
+  setup(ctx) {},
+};
 </script>
 
 <style lang="scss" scoped>
-
+.base-scroll-list {
+  width: 100%;
+  height: auto;
+  overflow: hidden;
+  .base-scroll-list-text {
+    padding: 0 10px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    box-sizing: border-box;
+  }
+  .base-scroll-list-header {
+    display: flex;
+    font-size: 15px;
+    align-items: center;
+    .header-item {
+    }
+  }
+}
 </style>
